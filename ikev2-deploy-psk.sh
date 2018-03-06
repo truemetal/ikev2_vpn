fa96aad246128c17a698e84e5f09891063ef3b31
Dannie P <true.metal.of.steel@gmail.com>
2018-03-06 11:26:41 +0200

    check for lsb_release presence

--- ikev2-deploy-psk.sh
+++ ikev2-deploy-psk.sh
@@ -1,5 +1,14 @@
-if [ $(lsb_release -i -s) != "Ubuntu" ] || [ $(lsb_release -r -s) != "16.04" ]; 
-        then echo -e "\033[31;7mThis script supports only Ubuntu 16.04. Terminating.\e[0m"; exit -1; 
+function bail_out {
+	echo -e "\033[31;7mThis script supports only Ubuntu 16.04. Terminating.\e[0m"
+	exit -1
+}
+
+if ! [ -x "$(command -v lsb_release)" ]; then
+	bail_out
+fi
+
+if [ $(lsb_release -i -s) != "Ubuntu" ] || [ $(lsb_release -r -s) != "16.04" ]; then 
+	bail_out
 fi
 
 export SHARED_KEY=$(uuidgen)
